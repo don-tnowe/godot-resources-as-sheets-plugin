cc47e7ef7338814d8723e7091a680ee3500aa572
darloth <darloth@pixelheaven.net>
2023-03-19 17:11:25 +0000

    Update table_pages.gd
    Basic handling of empty directory - just return without populating table.

--- addons/resources_spreadsheet_view/main_screen/table_pages.gd
+++ addons/resources_spreadsheet_view/main_screen/table_pages.gd
@@ -39,6 +39,8 @@ func _on_grid_updated():
 	var sort_type = node_editor_view_root.column_types[node_editor_view_root.columns.find(sort_property)]
 	var property_values = []
 	property_values.resize(page_count)
+	if(node_editor_view_root.rows.size() == 0):
+		return
 	for i in page_count:
 		property_values[i] = node_editor_view_root.rows[i * rows_per_page].get(sort_property)
 
