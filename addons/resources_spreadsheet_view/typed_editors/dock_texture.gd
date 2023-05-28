9f98bf6497b43498b669968533d9e72e4e25d44b
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2023-05-28 13:56:15 +0300

    Fix Texture dock... for which time already?

--- addons/resources_spreadsheet_view/typed_editors/dock_texture.gd
+++ addons/resources_spreadsheet_view/typed_editors/dock_texture.gd
@@ -4,7 +4,7 @@ extends SheetsDockEditor
 var _stored_value : Texture2D
 
 
-func try_edit_value(value, type, property_hint) -> bool:
+func try_edit_value(value, type, _property_hint) -> bool:
 	if type != TYPE_OBJECT or !value is Texture:
 		return false
 	
@@ -19,8 +19,7 @@ func _on_Button_pressed():
 	# No, Scene Unique Names can not be used in-editor (last time i checked)
 
 	var folder_name := _stored_value.resource_path.get_basename()
-	var dir := DirAccess.open(folder_name)
-	dir.make_dir(folder_name)
+	DirAccess.make_dir_recursive_absolute(folder_name)
 
 	var tex_size := _stored_value.get_size()
 	var tile_size := Vector2(tex_size.x / h_count, tex_size.y / v_count)
@@ -34,7 +33,7 @@ func _on_Button_pressed():
 			tile.take_over_path(folder_name + "/" + folder_name.get_file() + "_" + str(j * h_count + i + 1) + ".tres")
 			ResourceSaver.save(tile)
 
-	tile_array.resize(sheet.edited_cells.size())
+	tile_array.resize(sheet.get_edited_cells_values().size())
 	sheet.set_edited_cells_values(tile_array)
 	sheet.editor_plugin.get_editor_interface().get_resource_filesystem().scan()
 		
