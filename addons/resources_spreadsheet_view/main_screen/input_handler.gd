671229c085d33714a82f1d73b986a641f63335a5
left <lethefrost@gmail.com>
2025-03-07 03:58:37 -0500

    fix: macOS meta(cmd) key not handled correctly

--- addons/resources_spreadsheet_view/main_screen/input_handler.gd
+++ addons/resources_spreadsheet_view/main_screen/input_handler.gd
@@ -1,15 +1,21 @@
 @tool
 extends Node
 
-const TablesPluginEditorViewClass := preload("res://addons/resources_spreadsheet_view/editor_view.gd")
-const TablesPluginSelectionManagerClass := preload("res://addons/resources_spreadsheet_view/main_screen/selection_manager.gd")
-const TextEditingUtilsClass := preload("res://addons/resources_spreadsheet_view/text_editing_utils.gd")
+const TablesPluginEditorViewClass := preload(
+	"res://addons/resources_spreadsheet_view/editor_view.gd"
+)
+const TablesPluginSelectionManagerClass := preload(
+	"res://addons/resources_spreadsheet_view/main_screen/selection_manager.gd"
+)
+const TextEditingUtilsClass := preload(
+	"res://addons/resources_spreadsheet_view/text_editing_utils.gd"
+)
 
-@onready var editor_view : TablesPluginEditorViewClass = get_parent()
-@onready var selection : TablesPluginSelectionManagerClass = get_node("../SelectionManager")
+@onready var editor_view: TablesPluginEditorViewClass = get_parent()
+@onready var selection: TablesPluginSelectionManagerClass = get_node("../SelectionManager")
 
 
-func _on_cell_gui_input(event : InputEvent, cell_node : Control):
+func _on_cell_gui_input(event: InputEvent, cell_node: Control):
 	var cell := selection.get_cell_node_position(cell_node)
 	if event is InputEventMouseButton:
 		editor_view.grab_focus()
@@ -21,7 +27,10 @@ func _on_cell_gui_input(event : InputEvent, cell_node : Control):
 			selection.rightclick_cells()
 
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			if Input.is_key_pressed(KEY_CTRL):
+			# After checking event is an InputEventMouseButton,
+			# which extends InputEventWithModifiers,
+			# it should be safe to use is_command_or_control_pressed().
+			if event.is_command_or_control_pressed():
 				if cell in selection.edited_cells:
 					selection.deselect_cell(cell)
 
@@ -36,7 +45,7 @@ func _on_cell_gui_input(event : InputEvent, cell_node : Control):
 				selection.select_cell(cell)
 
 
-func _gui_input(event : InputEvent):
+func _gui_input(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed():
 			selection.rightclick_cells()
@@ -47,19 +56,26 @@ func _gui_input(event : InputEvent):
 				selection.deselect_all_cells()
 
 
-func _input(event : InputEvent):
+func _input(event: InputEvent):
 	if !event is InputEventKey or !event.pressed:
 		return
-	
+
 	if !editor_view.has_focus() or selection.edited_cells.size() == 0:
 		return
 
-	if event.keycode == KEY_CTRL or event.keycode == KEY_SHIFT:
-		# Modifier keys do not get processed.
+	if (
+		event.keycode == KEY_CTRL
+		or event.keycode == KEY_META
+		or event.keycode == KEY_SHIFT
+		or event.keycode == KEY_ALT
+	):
+		# If the modifier is the only key pressed, do nothing.
 		return
-	
+
 	# Ctrl + Z (before, and instead of, committing the action!)
-	if Input.is_key_pressed(KEY_CTRL):
+	# After checking event is a InputEventKey, which extends InputEventWithModifiers,
+	# it should be safe to use is_command_or_control_pressed().
+	if event.is_command_or_control_pressed():
 		if event.keycode == KEY_Z or event.keycode == KEY_Y:
 			return
 
@@ -68,23 +84,23 @@ func _input(event : InputEvent):
 	editor_view.editor_interface.get_resource_filesystem().scan()
 
 
-func _key_specific_action(event : InputEvent):
+func _key_specific_action(event: InputEvent):
 	var column := selection.get_cell_column(selection.edited_cells[0])
-	var ctrl_pressed := Input.is_key_pressed(KEY_CTRL)
+	var ctrl_pressed: bool = event.is_command_or_control_pressed()
 
 	# BETWEEN-CELL NAVIGATION
-	var grid_move_offset := (10 if ctrl_pressed else 1)
+	var grid_move_offset := 10 if ctrl_pressed else 1
 	if event.keycode == KEY_UP:
 		_move_selection_on_grid(0, -grid_move_offset)
 
 	elif event.keycode == KEY_DOWN:
-		_move_selection_on_grid(0, +grid_move_offset)
+		_move_selection_on_grid(0, grid_move_offset)
 
 	elif Input.is_key_pressed(KEY_SHIFT) and event.keycode == KEY_TAB:
 		_move_selection_on_grid(-grid_move_offset, 0)
-	
+
 	elif event.keycode == KEY_TAB:
-		_move_selection_on_grid(+grid_move_offset, 0)
+		_move_selection_on_grid(grid_move_offset, 0)
 
 	elif ctrl_pressed and event.keycode == KEY_C:
 		TextEditingUtilsClass.multi_copy(selection.edited_cells_text)
@@ -97,41 +113,60 @@ func _key_specific_action(event : InputEvent):
 
 	# TEXT CARET MOVEMENT
 	var caret_move_offset := TextEditingUtilsClass.get_caret_movement_from_key(event.keycode)
-	if TextEditingUtilsClass.multi_move_caret(caret_move_offset, selection.edited_cells_text, selection.edit_cursor_positions, ctrl_pressed):
+	if TextEditingUtilsClass.multi_move_caret(
+		caret_move_offset,
+		selection.edited_cells_text,
+		selection.edit_cursor_positions,
+		ctrl_pressed
+	):
 		selection.queue_redraw()
 		return
 
 	# The following actions do not work on non-editable cells.
-	if !selection.column_editors[column].is_text() or editor_view.columns[column] == "resource_path":
+	if (
+		!selection.column_editors[column].is_text()
+		or editor_view.columns[column] == "resource_path"
+	):
 		return
-	
+
 	# ERASING
-	elif event.keycode == KEY_BACKSPACE:
-		editor_view.set_edited_cells_values_text(TextEditingUtilsClass.multi_erase_left(
-			selection.edited_cells_text, selection.edit_cursor_positions, ctrl_pressed
-		))
+	if event.keycode == KEY_BACKSPACE:
+		editor_view.set_edited_cells_values_text(
+			TextEditingUtilsClass.multi_erase_left(
+				selection.edited_cells_text, selection.edit_cursor_positions, ctrl_pressed
+			)
+		)
 
 	elif event.keycode == KEY_DELETE:
-		editor_view.set_edited_cells_values_text(TextEditingUtilsClass.multi_erase_right(
-			selection.edited_cells_text, selection.edit_cursor_positions, ctrl_pressed
-		))
+		editor_view.set_edited_cells_values_text(
+			TextEditingUtilsClass.multi_erase_right(
+				selection.edited_cells_text, selection.edit_cursor_positions, ctrl_pressed
+			)
+		)
 		get_viewport().set_input_as_handled()
 
 	# And finally, text typing.
-	elif event.keycode == KEY_ENTER:
-		editor_view.set_edited_cells_values_text(TextEditingUtilsClass.multi_input(
-			"\n", selection.edited_cells_text, selection.edit_cursor_positions
-		))
+	elif !ctrl_pressed:
+		if event.keycode == KEY_ENTER:
+			editor_view.set_edited_cells_values_text(
+				TextEditingUtilsClass.multi_input(
+					"\n", selection.edited_cells_text, selection.edit_cursor_positions
+				)
+			)
 
-	elif event.unicode != 0 and event.unicode != 127:
-		editor_view.set_edited_cells_values_text(TextEditingUtilsClass.multi_input(
-			char(event.unicode), selection.edited_cells_text, selection.edit_cursor_positions
-		))
+		elif event.unicode != 0 and event.unicode != 127:
+			editor_view.set_edited_cells_values_text(
+				TextEditingUtilsClass.multi_input(
+					char(event.unicode),
+					selection.edited_cells_text,
+					selection.edit_cursor_positions
+				)
+			)
 
 	selection.queue_redraw()
 
 
-func _move_selection_on_grid(move_h : int, move_v : int):
+func _move_selection_on_grid(move_h: int, move_v: int):
 	var selected_cells := selection.edited_cells.duplicate()
 	var num_columns := editor_view.columns.size()
 	var num_rows := editor_view.rows.size()
