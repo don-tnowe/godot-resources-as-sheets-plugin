2b2a4f2922f3c37867218788b456ea6b5bd0b24b
Korwin Briggs <Korwin.Briggs@gmail.com>
2025-05-05 23:26:07 -0400

    copied in code for word-wrap in string cells

--- addons/resources_spreadsheet_view/typed_cells/cell_editor_string.gd
+++ addons/resources_spreadsheet_view/typed_cells/cell_editor_string.gd
@@ -1,6 +1,18 @@
 extends ResourceTablesCellEditor
 
 
+func create_cell(caller : Control) -> Control:
+	var cell_scene: Label = load(CELL_SCENE_DIR + "basic.tscn").instantiate()
+	cell_scene.resized.connect(_resize_text.bind(cell_scene))
+	return cell_scene
+
+func _resize_text(cell: Label):
+	if cell.text.length() <= 65:
+		cell.autowrap_mode = TextServer.AUTOWRAP_OFF
+	else:
+		cell.autowrap_mode = TextServer.AUTOWRAP_WORD
+		cell.custom_minimum_size.x = 350
+
 func to_text(value) -> String:
 	return str(value)
 
